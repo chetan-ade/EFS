eeb819e679dabb4aea622aba38b5956a6f1b3da3
Chetan Ade <chetan.ade98@gmail.com>
2019-03-15 11:36:27 +0530

    registered success

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,15 +6,14 @@ const url = "mongodb+srv://chetanade:improve619619@efs-zym9f.azure.mongodb.net/t
 
 http.createServer(function (req, res) {
     if (req.url == '/fileuploaded') {
-        //file upload page
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
             var oldpath = files.filetoupload.path;
             var newpath = 'C:/Users/Chetan/Desktop/EFSstorage/' + files.filetoupload.name;
             fs.rename(oldpath, newpath, function (err) {
                 if (err) throw err;
-                fs.readFile('./html/fileUploaded.html', function(err, data) {
-                    res.writeHead(200, {'Content-Type': 'text/html'});
+                fs.readFile('./html/fileUploaded.html', function (err, data) {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
                     res.write(data);
                     res.end();
                 });
@@ -22,51 +21,54 @@ http.createServer(function (req, res) {
         });
     } else if (req.url == '/uploadFile') {
         //File upload form
-        fs.readFile('./html/uploadFile.html', function(err, data) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile('./html/uploadFile.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(data);
             res.end();
-          });
+        });
     } else if (req.url == '/') {
         //main page
-        fs.readFile('./html/main.html', function(err, data) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile('./html/main.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(data);
             res.end();
-          });
+        });
     } else if (req.url == '/register') {
-        fs.readFile('./html/register.html', function(err, data) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile('./html/register.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(data);
             res.end();
-          });
+        });
     } else if (req.url == '/login') {
-        fs.readFile('./html/login.html', function(err, data) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile('./html/login.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(data);
             res.end();
-          });
+        });
     } else if (req.url == '/registerSuccess') {
         //insertInCollection.js
-        MongoClient.connect(url, function(err, db) {
-          if (err) throw err;
-          var dbo = db.db("EFSDB");
-        //   var email = form.email;
-        //   var password = form.password;
-        //   console.log(email,password);
-          var myobj = { email: "Chetan", password: "qwer" };
-          dbo.collection("users").insertOne(myobj, function(err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-            db.close();
-          });
-        }); 
-
-        fs.readFile('./html/registerSuccess.html', function(err, data) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            res.end();
-          });
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            var emailInput = fields.email;
+            var passwordInput = fields.password;
+            // console.log("Email: ", emailInput);
+            // console.log("password: ", passwordInput);
+            MongoClient.connect(url, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("EFSDB");
+                var myobj = { email: emailInput, password: passwordInput };
+                dbo.collection("users").insertOne(myobj, function (err, res) {
+                    if (err) throw err;
+                    console.log("1 document inserted");
+                    db.close();
+                });
+            });
+            fs.readFile('./html/registerSuccess.html', function (err, data) {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data);
+                res.end();
+            });
+        });
     }
 }).listen(8080);
 
