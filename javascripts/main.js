f60c8b0d4f83681e83b3c079190f5aaa9efb6efa
Chetan Ade <chetan.ade98@gmail.com>
2019-04-17 13:32:27 +0530

    sharing implemented

--- javascripts/main.js
+++ javascripts/main.js
@@ -72,9 +72,39 @@ http.createServer(function (req, res) {
                                         res.write('<input type="submit" name="filename" value="' + filename + '"/>');
                                         res.write('<br><br>');
                                     }
-                                    res.write('</form');
+                                    res.write('</form>');
                                 } else {
                                     res.write('<h1>No files uploaded...</h1>');
+                                }
+                                res.write('<h3>Files Sharing</h3>');
+                                res.write('<br>');
+                                if (result.filesOwned != undefined) {
+                                    res.write('<form action="./shareFile" method="post" enctype="multipart/form-data">');
+                                    res.write('<input type="hidden" name="email" value="' + emailInput + '">');
+                                    res.write('<input type="text" name="filetoshare" placeholder="Enter file name to share">');
+                                    res.write('<br>');
+                                    res.write('<br>');
+                                    res.write('<input type="text" name="persontoshare" placeholder="Enter email address">');
+                                    res.write('<br>');
+                                    res.write('<br>');
+                                    res.write('<input type="submit" >');
+                                    res.write('</form>');
+                                }
+                                else {
+                                    res.write('<h5>No files available to share<h5>');
+                                }
+                                res.write('<h3>Files Shared With Me</h3>');
+                                res.write('<form action="./downloadSharedFile" method="post" enctype="multipart/form-data">');
+                                // res.write('<input type="hidden" name="email" value="' + emailInput + '">');
+                                if (result.filesSharedWithMe != undefined) {
+                                    for (var i = 0; i < result.filesSharedWithMe.length; i++) {
+                                        filename = result.filesSharedWithMe[i].filename;
+                                        res.write('<input type="submit" name="filename" value="' + filename + ':' + result.filesSharedWithMe[i].owner + '"/>');
+                                        res.write('<br><br>');
+                                    }
+                                    res.write('</form>');
+                                } else {
+                                    res.write('<h1>No files shared with me...</h1>');
                                 }
                                 res.write('</body>');
                                 res.write('<script>alert("login successfully");</script>');
@@ -193,6 +223,7 @@ http.createServer(function (req, res) {
     } else if (req.url == '/fileuploaded') {
         //file successfully uploaded
         var form = new formidable.IncomingForm();
+        var newfilepath;
         form.parse(req, function (err, fields, files) {
             var email = fields.email;
             MongoClient.connect(url, {
@@ -210,13 +241,13 @@ http.createServer(function (req, res) {
                 };
                 dbo.collection("users").updateOne(myquery, newvalues, function (err, res) {
                     if (err) throw err;
-                    console.log('1 file added to filesOwned');
+                    console.log('1 file added to filesOwnedDB');
                     db.close();
                 });
+
                 var oldpath = files.filetoupload.path;
-                var newpath = 'C:/Users/Chetan/Desktop/EFS/storage/' + email + '/' + files.filetoupload.name;
-                console.log('1 file uploaded to filesystem');
-                fs.rename(oldpath, newpath, function (err) {
+                newfilepath = 'C:/Users/Chetan/Desktop/EFS/storage/' + email + '/' + files.filetoupload.name;
+                fs.rename(oldpath, newfilepath, function (err) {
                     if (err) throw err;
                     MongoClient.connect(url, {
                         useNewUrlParser: true
@@ -244,7 +275,37 @@ http.createServer(function (req, res) {
                                     res.write('<input type="submit" name="filename" value="' + filename + '"/>');
                                     res.write('<br><br>');
                                 }
-                                res.write('</form');
+                                res.write('</form>');
+                                res.write('<h3>Files Sharing</h3>');
+                                res.write('<br>');
+                                if (result.filesOwned != undefined) {
+                                    res.write('<form action="./shareFile" method="post" enctype="multipart/form-data">');
+                                    res.write('<input type="hidden" name="email" value="' + email + '">');
+                                    res.write('<input type="text" name="filetoshare" placeholder="Enter file name to share">');
+                                    res.write('<br>');
+                                    res.write('<br>');
+                                    res.write('<input type="text" name="persontoshare" placeholder="Enter email address">');
+                                    res.write('<br>');
+                                    res.write('<br>');
+                                    res.write('<input type="submit" >');
+                                    res.write('</form>');
+                                }
+                                else {
+                                    res.write('<h5>No files available to share<h5>');
+                                }
+                                res.write('<h3>Files Shared With Me</h3>');
+                                res.write('<form action="./downloadSharedFile" method="post" enctype="multipart/form-data">');
+                                // res.write('<input type="hidden" name="email" value="' + emailInput + '">');
+                                if (result.filesSharedWithMe != undefined) {
+                                    for (var i = 0; i < result.filesSharedWithMe.length; i++) {
+                                        filename = result.filesSharedWithMe[i].filename;
+                                        res.write('<input type="submit" name="filename" value="' + filename + ':' + result.filesSharedWithMe[i].owner + '"/>');
+                                        res.write('<br><br>');
+                                    }
+                                    res.write('</form>');
+                                } else {
+                                    res.write('<h1>No files shared with me...</h1>');
+                                }
                                 res.write('</body>');
                                 console.log('file uploaded successfully');
                                 res.write('<script>alert("file uploaded successfully");</script>');
@@ -255,17 +316,77 @@ http.createServer(function (req, res) {
                         });
                     });
                 });
+                // // Encrypt file.
+                // var newfilepathDAT = newfilepath.split(".")[0] + '.dat';
+                // encryptor.encryptFile(newfilepath, newfilepathDAT, key, function (err) {
+                //     console.log('file encrypted');
+                //     console.log('source: ',newfilepath);
+                //     console.log('dest: ',newfilepathDAT);
+                // });
             });
-            // // Encrypt file.
-            // encryptor.encryptFile(newpath,newpath, key, function(err) {
-            //     // Encryption complete.
-            // });
+
         });
     } else if (req.url == '/downloadFile') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
             var email = fields.email;
             var filename = fields.filename;
+            console.log(email);
+            console.log(filename);
+            var filepath = 'C:/Users/Chetan/Desktop/EFS/storage/' + email + '/' + filename;
+            console.log(filepath);
+            var newfilepath = 'C:/Users/Chetan/Desktop/EFS/storage/' + email + '/' + 'DEC' + filename;
+            // encryptor.decryptFile(filepath, newfilepath, key, function(err) {
+            //     // Decryption complete.
+            //   });
+            fs.readFile(filepath, function (err, data) {
+                res.writeHead(200, {
+                    'Content-Disposition': 'attachment;filename=' + filename
+                });
+                res.write(data);
+                res.end();
+            });
+        });
+    } else if (req.url == '/shareFile') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            var owner = fields.email;
+            var filename = fields.filetoshare;
+            var persontoshare = fields.persontoshare;
+            console.log(owner);
+            console.log(filename);
+            console.log(persontoshare);
+            MongoClient.connect(url, {
+                useNewUrlParser: true
+            }, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("EFSDB");
+                var myquery = {
+                    _id: persontoshare
+                };
+                var newvalues = {
+                    $push: {
+                        filesSharedWithMe: {
+                            filename,
+                            owner
+                        },
+                    }
+                };
+                dbo.collection("users").updateOne(myquery, newvalues, function (err, res) {
+                    if (err) throw err;
+                    console.log('1 file added to filesSharedDB');
+                    db.close();
+                });
+            });
+        });
+        res.write('<script>alert("shared successfully");</script>');
+        res.end();
+    } else if (req.url == '/downloadSharedFile') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            var file = fields.filename;
+            var filename = file.split(':')[0];
+            var email = file.split(':')[1];
             console.log(email);
             console.log(filename);
             var filepath = 'C:/Users/Chetan/Desktop/EFS/storage/' + email + '/' + filename;
